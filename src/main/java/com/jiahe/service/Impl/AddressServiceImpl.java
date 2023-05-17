fc13430e6a3cc24795d681bed43636b783a2e698
Shiay6Lan <2499073963@qq.com>
2023-05-17 20:04:17 +0800

    登录修改

--- src/main/java/com/jiahe/service/Impl/AddressServiceImpl.java
+++ src/main/java/com/jiahe/service/Impl/AddressServiceImpl.java
@@ -4,9 +4,12 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.jiahe.dao.AddressDao;
 import com.jiahe.pojo.Address;
+import com.jiahe.pojo.Users;
 import com.jiahe.service.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AddressServiceImpl implements AddressService {
@@ -19,6 +22,11 @@ public class AddressServiceImpl implements AddressService {
     public IPage<Address> selectByPage(Integer current, Integer size) {
         IPage<Address> page = new Page<Address>(current, size);
         return addressDao.selectPage(page, null);
+    }
+
+    @Override
+    public List<Address> searchPersonalAddress(Users user) {
+        return null;
     }
 
 
