d0b0c8e36a75492b5aeef8d27d846b453fd02c31
N9Way <1400724242@qq.com>
2023-05-17 16:31:21 +0800

    tr 前台地址界面

---
+++ src/main/java/com/jiahe/service/Impl/AddressServiceImpl.java
@@ -0,0 +1,25 @@
+package com.jiahe.service.Impl;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.jiahe.dao.AddressDao;
+import com.jiahe.pojo.Address;
+import com.jiahe.service.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+
+    @Autowired
+    private AddressDao addressDao;
+
+    //分页查询
+    @Override
+    public IPage<Address> selectByPage(Integer current, Integer size) {
+        IPage<Address> page = new Page<Address>(current, size);
+        return addressDao.selectPage(page, null);
+    }
+
+
+}
